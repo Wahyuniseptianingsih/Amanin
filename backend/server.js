70340470a4429c71aa671d0cf334921d93000afc
Wahyuni Septianingsih <wahyuniseptianingsih07@gmail.com>
2026-08-30 03:08:03 +0700

    tambah dockerfile buat back4app

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import { hitungSkor } from "./checks/scoring.js";
 import pool from "./db.js";
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 
